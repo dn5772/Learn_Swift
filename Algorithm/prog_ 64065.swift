0088b850e1a80b67c667a617df96596acf625aa9
dn5772 <shendanny@naver.com>
2022-09-24 21:30:30 +0900

    clear 64065

---
+++ Algorithm/prog_ 64065.swift
@@ -0,0 +1,42 @@
+import Foundation
+
+extension String {
+    func remove(target string: String) -> String {
+        return components(separatedBy: string).joined()
+    }
+}
+
+let ascending: (Substring, Substring) -> Bool = {(lhs, rhs) in
+    return lhs.count < rhs.count
+}
+
+func solution(_ s:String) -> [Int] {
+    var str :String = s
+    
+    str = str.remove(target: "},")
+    str = str.remove(target: "}}")
+    
+    var strList = str.split(separator: "{")
+    strList.sort(by: ascending)
+    
+    var intList :[[Int]] = []
+    var ans :[Int] = []
+    
+    for subStr in strList {
+        var arr = subStr.split(separator: ",")
+        intList.append(arr.map { Int($0)!})
+    }
+    
+    for arr in intList{
+        for itme in arr{
+            if (!ans.contains(itme)){
+                ans.append(itme)
+            }
+        }
+    }
+    return ans
+}
+
+print(
+    solution("{{2},{2,1,3,4,5},{2,1},{2,1,3},{2,1,3,4}}")
+)
