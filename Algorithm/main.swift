84b041332594b421260d89cd8b6238a463c394aa
dn5772 <shendanny@naver.com>
2022-09-24 19:25:19 +0900

    22.9.24 19:25

--- Algorithm/main.swift
+++ Algorithm/main.swift
@@ -1,2 +1,24 @@
 import Foundation
 
+extension String {
+    func remove(target string: String) -> String {
+        return components(separatedBy: string).joined()
+    }
+}
+
+func solution(_ s:String) -> [Int] {
+    var str :String = s
+    
+    str = str.remove(target: "},")
+    str = str.remove(target: "}}")
+    
+    var strs = str.split(separator: "{")
+    
+    print(type(of: strs))
+    
+    print(strs)
+
+    return []
+}
+
+solution("{{2},{2,1},{2,1,3},{2,1,3,4}}")
