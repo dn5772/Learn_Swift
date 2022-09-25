a36a2c6746a92feb0886935bd1234d68d7ce4f5f
dn5772 <shendanny@naver.com>
2022-09-25 18:53:11 +0900

    add func count

--- Algorithm/main.swift
+++ Algorithm/main.swift
@@ -1,18 +1,38 @@
 import Foundation
 
+func counting(_ d : Dictionary<String, Int>, _ sets :Set<String>) -> Int{
+    var cnt = 0
+    for set in sets {
+        if d[set] != 0{
+            cnt += 1
+        }
+    }
+    return cnt
+}
+
 func solution(_ gems:[String]) -> [Int] {
     var sets :Set<String> = []
+    var ans :[Int] = []
     var dictionary :Dictionary<String, Int> = [:]
     
-    for (index, s) in gems.enumerated(){
+    for s in gems{
         if (!sets.contains(s)){
             sets.insert(s)
-            dictionary[s] = index
+            dictionary[s] = 0
         }
     }
     
+    var start: Int = 0, end: Int = 0
+    var count = sets.count
     
-    return []
+    while (end <= count) && (start < count) {
+        
+    }
+    
+    ans.append(start+1)
+    ans.append(end)
+    
+    return ans
 }
 
 print(solution(["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"]))
