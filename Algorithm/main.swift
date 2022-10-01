7381f809b9a94a9f4b02b237477940fcdae5ff2e
dn5772 <shendanny@naver.com>
2022-10-01 22:49:13 +0900

    보석쇼핑 마무리

--- Algorithm/main.swift
+++ Algorithm/main.swift
@@ -1,55 +1,34 @@
 import Foundation
 
-func typeCounting(_ d : Dictionary<String, Int>, _ s :Set<String>) -> Int{
-    var cnt = 0
-    for set in s {
-        if d[set] != 0{
-            cnt += 1
-        }
-    }
-    return cnt
-}
 
 func solution(_ gems:[String]) -> [Int] {
-    var sets :Set<String> = []
     var dictionary :Dictionary<String, Int> = [:]
-    var indexs :[Int] = []
-    
-    var newGems = gems
-    newGems.append("")
-    
-    for index in 0..<(newGems.count-1){
-        let gem = gems[index]
-        if (!sets.contains(gem)){
-            sets.insert(gem)
-            dictionary[gem] = 0
-        }
-        if gem != newGems[index+1]{
-            indexs.append(index)
-        }
-    }
+    let sets = Set(gems)
     
     var end: Int = 0
-    var i = 0
     let gemType = sets.count
     let gemCount = gems.count
     var tuple: (Int, Int) = (0, gemCount)
     
-    dictionary[gems[0]] = dictionary[gems[0]]! + 1
-    
-    for start in indexs{
-        while (i < (indexs.count - 1)) && (typeCounting(dictionary, sets) != gemType){
-            i += 1
-            end = indexs[i]
-            dictionary[gems[end]] = dictionary[gems[end]]! + 1
+    dictionary[gems[0], default: 0] += 1
+    for (start, gem) in gems.enumerated() {
+        while (end < gemCount-1) && (dictionary.count != gemType){
+            end += 1
+            dictionary[gems[end], default: 0] += 1
         }
-        if (typeCounting(dictionary, sets) == gemType) {
+    
+        if (dictionary.count == gemType) {
             if (end - start) < (tuple.1 - tuple.0){
                 tuple.0 = start
                 tuple.1 = end
             }
         }
-        dictionary[gems[start]] = dictionary[gems[start]]! - 1
+        
+        dictionary[gem, default: 0] -= 1
+        if dictionary[gem] == 0{
+            dictionary.removeValue(forKey: gem)
+        }
+        
     }
     
     return [tuple.0 + 1, tuple.1 + 1]
