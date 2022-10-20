6242a526af153d8839143ff45fa20f13b8e3954b
dn5772 <shendanny@naver.com>
2022-10-20 21:30:19 +0900

    64062 start

--- Algorithm/main.swift
+++ Algorithm/main.swift
@@ -1,39 +1,9 @@
 import Foundation
 
-func solution(_ tickets:[[String]]) -> [String] {
-    var result: [String] = []
+func solution(_ stones:[Int], _ k:Int) -> Int {
+    var answer: Int = 0
     
-    let sortedTickets = tickets.sorted(by: {(v1:[String], v2:[String])->Bool in
-        if v1[0] == v2[0]{return v1[1] < v2[1]}
-        return v1[0] < v2[0]
-    })
-
-    result.append("ICN")
     
-    func travle(_ arr: [[String]], _ startCity: String){
-        if result.count == tickets.count+1{
-            return
-        }
-        if !arr.isEmpty {
-            for (index, city) in arr.enumerated(){
-                if city[0] == startCity{
-                    if arr.contains(where: {$0[0]==city[1]})||arr.count==1{
-                        var newArr = arr
-                        newArr.remove(at: index)
-                        result.append(city[1])
-                        travle(newArr, city[1])
-                        if result.count != tickets.count+1{
-                            result.removeLast()
-                        }
-                    }
-                }
-            }
-        }
-    }
     
-    travle(sortedTickets, "ICN")
-    
-    return result
+    return answer
 }
-
-print(solution([["ICN", "A"], ["A", "B"], ["A", "C"], ["C", "A"], ["B", "D"]]))
