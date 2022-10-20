1036fc6a66bf5d017b098223104c96705680b6c9
dn5772 <shendanny@naver.com>
2022-10-20 21:28:10 +0900

    ticket end

--- Algorithm/main.swift
+++ Algorithm/main.swift
@@ -2,25 +2,38 @@ import Foundation
 
 func solution(_ tickets:[[String]]) -> [String] {
     var result: [String] = []
-    var sortedTickets = tickets.sorted(by: {(v1:[String], v2:[String])->Bool in
+    
+    let sortedTickets = tickets.sorted(by: {(v1:[String], v2:[String])->Bool in
         if v1[0] == v2[0]{return v1[1] < v2[1]}
         return v1[0] < v2[0]
     })
+
+    result.append("ICN")
     
-    var lastCity: String = "ICN"
-    result.append(lastCity)
-    
-    while !sortedTickets.isEmpty{
-        for (index, city) in sortedTickets.enumerated() {
-            if city[0] == lastCity{
-                result.append(city[1])
-                lastCity = city[1]
-                sortedTickets.remove(at: index)
-                break
+    func travle(_ arr: [[String]], _ startCity: String){
+        if result.count == tickets.count+1{
+            return
+        }
+        if !arr.isEmpty {
+            for (index, city) in arr.enumerated(){
+                if city[0] == startCity{
+                    if arr.contains(where: {$0[0]==city[1]})||arr.count==1{
+                        var newArr = arr
+                        newArr.remove(at: index)
+                        result.append(city[1])
+                        travle(newArr, city[1])
+                        if result.count != tickets.count+1{
+                            result.removeLast()
+                        }
+                    }
+                }
             }
         }
     }
+    
+    travle(sortedTickets, "ICN")
+    
     return result
 }
 
-print(solution([["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]))
+print(solution([["ICN", "A"], ["A", "B"], ["A", "C"], ["C", "A"], ["B", "D"]]))
